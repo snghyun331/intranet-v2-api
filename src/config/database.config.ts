1b94f176645caf46dcf9bfdc09fee7246b873d9d
LeeSH <snghyun331@gmail.com>
2024-12-27 10:48:31 +0900

    Test: [#121] 식대(user) e2e 추가 Ver.1

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -16,3 +16,19 @@ export const DATABASE_CONFIG: TypeOrmModuleAsyncOptions = {
     synchronize: true,
   }),
 };
+
+export const DATABASE_CONFIG_TEST: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'mariadb',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USER'),
+    password: configService.get<string>('DB_PW'),
+    database: configService.get<string>('DB_NAME'),
+    charset: 'utf8mb4',
+    entities: [__dirname + '/../**/*.entity.{ts,js}'],
+    synchronize: true,
+  }),
+};
