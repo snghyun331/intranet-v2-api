6d0d06e97abb4d3ddb532b49b80aebb94150643e
LeeSH <snghyun331@gmail.com>
2024-12-30 10:33:09 +0900

    Refactor: [#282] Testing App 초기화 로직 모듈화

--- test/meal/meal.e2e.spec.ts
+++ test/meal/meal.e2e.spec.ts
@@ -1,52 +1,22 @@
 import { INestApplication } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { MealModule } from '../../src/module/meal/meal.module';
 import { CreateMealDto } from '../../src/module/meal/dto/createMeal.dto';
 import { AttendanceEnum } from '../../src/common/constant/enum';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DATABASE_CONFIG_TEST } from '../../src/config/database.config';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from '../../src/module/auth/auth.module';
-import { UserModule } from '../../src/module/user/user.module';
-import { JwtService } from '@nestjs/jwt';
 import { MealStats } from '../../src/module/meal/interface/meal.interface';
+import { closeTestApp, createTestAppWithMeal } from '../test-utility';
 
 describe('UserMealController (e2e)', () => {
   let app: INestApplication;
   let accessToken: string;
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        TypeOrmModule.forRootAsync(DATABASE_CONFIG_TEST),
-        AuthModule,
-        MealModule,
-        UserModule,
-      ],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    const jwtService: JwtService = app.get(JwtService);
-
-    accessToken = jwtService.sign({
-      userIdx: 1,
-      userName: '이승현',
-      userGender: 'W',
-      userBirth: '2000-03-31',
-      joinDate: '2004-03-04',
-      hqName: 'HR솔루션본부',
-      teamName: 'HR Tech',
-      gradeName: '본부장',
-      adminRole: 'Y',
-    });
-
-    await app.init();
+    const result = await createTestAppWithMeal();
+    app = result.app;
+    accessToken = result.userAccessToken;
   });
 
   afterAll(async () => {
-    await app.close();
+    await closeTestApp(app);
   });
 
   describe('[POST] /users/meals', () => {
