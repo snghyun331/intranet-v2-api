31a870114e07f09038962dbbcbbd76dfd6ba8865
LeeSH <snghyun331@gmail.com>
2025-02-07 11:59:05 +0900

    Modify: [#372] 출근 상태에서 출근 시간만 조정 가능하도록 로직 추가

--- src/module/intranet/commute/dto/updateCommuteTime.dto.ts
+++ src/module/intranet/commute/dto/updateCommuteTime.dto.ts
@@ -1,16 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateCommuteTimeDto {
   @ApiProperty({ type: Date, description: '출근 시간(타임스탬프 형식, UTC 기준)', required: true })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '출근 시간은 반드시 입력 후 수정해주세요.' })
   @Type(() => Date)
   @IsDate()
   checkInTime: Date;
 
   @ApiProperty({ type: Date, description: '퇴근 시간(타임스탬프 형식, UTC 기준)', required: true })
-  @IsNotEmpty()
+  @IsOptional()
   @Type(() => Date)
   @IsDate()
   checkOutTime: Date;
