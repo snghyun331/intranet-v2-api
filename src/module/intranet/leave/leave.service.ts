36f0d068a631605535c7d89a5cf503b5dcafa905
LeeSH <snghyun331@gmail.com>
2025-03-12 14:48:09 +0900

    Modify: [#445] CEO 휴가 등록 시 자동승인 로직 추가

--- src/module/intranet/leave/leave.service.ts
+++ src/module/intranet/leave/leave.service.ts
@@ -4,7 +4,7 @@ import { LeaveRepository } from './repository/leave.repository';
 import { EntityManager } from 'typeorm';
 import { LeaveRequestDto } from './dto/createLeave.dto';
 import { ConfigService } from '@nestjs/config';
-import { ConfirmEnum, IntranetLeaveTypeIdxEnum, NodeEnvEnum } from '../../../common/constant/enum';
+import { ConfirmEnum, IntranetLeaveTypeIdxEnum, NodeEnvEnum, UserGradeEnum } from '../../../common/constant/enum';
 import { AwsService } from '../../aws/aws.service';
 import { LeaveImageInfo, LeaveSummary } from './interface/leave.interface';
 import { PageNoDto } from '../../../common/dto/pageNo.dto';
@@ -18,6 +18,7 @@ import {
   SPECIAL_LEAVE_LISTS,
   TRAINING_LEAVE_LISTS,
 } from '../../../common/constant/constant';
+import { UserPayload } from '../../../common/interface/payload.interface';
 
 @Injectable()
 export class LeaveService {
@@ -29,13 +30,58 @@ export class LeaveService {
 
   async createLeave(
     dto: LeaveRequestDto,
-    userIdx: number,
+    user: UserPayload,
     manager: EntityManager,
     leaveImage?: Express.Multer.File,
   ): Promise<void> {
+    const userIdx: number = user.userIdx;
     const { leaveInfo, approverIdxs, note } = dto;
     const nowYear: number = moment().utcOffset(9).year();
     const nowMonth: number = moment().utcOffset(9).month() + 1;
+
+    // CEO이면, 아무 조건 없이 휴가 등록 및 자동승인
+    if (user.gradeName === UserGradeEnum.CEO) {
+      await Promise.all(
+        leaveInfo.map(async (leave) => {
+          const dateStringFormat: RegExp = /^\d{4}-\d{2}-\d{2}$/;
+          if (!dateStringFormat.test(leave.commuteDate)) {
+            throw new BadRequestException('commuteDate는 0000-00-00 날짜 형식으로 입력해주세요');
+          }
+          if (!Object.values(IntranetLeaveTypeIdxEnum).includes(leave.leaveTypeIdx)) {
+            throw new BadRequestException('올바른 휴가유형 IDX을 입력해주세요.');
+          }
+
+          // 휴가등록
+          const commuteIdx: number = await this.leaveRepository.createLeave(leave, userIdx, note, manager);
+          if (leaveImage) {
+            const env: string = this.configService.get<string>('NODE_ENV');
+            const rootDir: string = env === NodeEnvEnum.TEST ? 'TEST' : 'PROD';
+            // 1. S3에 저장
+            const { buffer, mimetype } = leaveImage;
+            const bucketName: string = this.configService.get<string>('S3_BUCKET_NAME');
+            const fileName: string = mimetype === 'application/pdf' ? 'proof.pdf' : `proof.${mimetype.split('/')[1]}`;
+            const uploadS3FilePath: string = `${rootDir}/LEAVE/${commuteIdx}/${fileName}`;
+            const imageUrl: string = await this.awsService.uploadImageToS3(
+              bucketName,
+              uploadS3FilePath,
+              buffer,
+              mimetype,
+            );
+            const imageInfo: LeaveImageInfo = {
+              imageName: fileName,
+              imageSize: leaveImage.size,
+              imageUrl,
+            };
+            // 2. DB에 저장
+            await this.leaveRepository.createLeaveImage(commuteIdx, imageInfo, manager);
+          }
+          // 자동승인
+          await this.leaveRepository.autoApprove(commuteIdx, userIdx, manager);
+        }),
+      );
+
+      return;
+    }
 
     await Promise.all(
       leaveInfo.map(async (leave) => {
