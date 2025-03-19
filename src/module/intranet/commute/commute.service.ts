3f48cdbb1fbb3b5935a713b61ec6dc57d203b197
LeeSH <snghyun331@gmail.com>
2025-03-19 11:03:45 +0900

    Modify: [#462] git Action으로 e2e 실행 시 테스트 순차 실행 처리(test -ing)

--- src/module/intranet/commute/commute.service.ts
+++ src/module/intranet/commute/commute.service.ts
@@ -170,13 +170,15 @@ export class CommuteService {
     }
 
     const workingMinutes: number = (finalCheckOutTime.getTime() - finalCheckInTime.getTime()) / (1000 * 60);
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+    console.log(workingMinutes);
     const overtimeWorkingMinutes: number =
       workingMinutes > standardWorkingMinutes ? Math.floor(workingMinutes - standardWorkingMinutes) : 0;
 
     /* 최종 근무시간이 기준시간 미만이면 사유를 필수 입력 */
-    // if (workingMinutes < standardWorkingMinutes && !checkOutDto.earlyLeaveReason) {
-    //   throw new BadRequestException('조기퇴근 시, 사유 입력은 필수입니다.');
-    // }
+    if (workingMinutes < standardWorkingMinutes && !checkOutDto.earlyLeaveReason) {
+      throw new BadRequestException('조기퇴근 시, 사유 입력은 필수입니다.');
+    }
 
     /* 근태 상태 설정 */
     let attendance: IntranetAttendanceEnum;
