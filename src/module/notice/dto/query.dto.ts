7a409707a43b3efcbf8f0e8893ce52334f671188
LeeSH <snghyun331@gmail.com>
2025-04-18 15:41:28 +0900

    Modify: [#611] ADMIN 공지사항 검색 쿼리 추가 Ver.2

--- src/module/notice/dto/query.dto.ts
+++ src/module/notice/dto/query.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AdminNoticeFilterDto {
-  @ApiProperty({ type: String, required: false, description: '제목 검색' })
-  title?: string;
+  @ApiProperty({ type: String, required: false, description: '제목 및 내용 검색' })
+  searchWord?: string;
 }
